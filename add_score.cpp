6404715010b801e8428349543ec8fd5f2a7d5962
Dow Perkins <walter21@vt.edu>
2017-05-18 11:27:40 -0400

    created an add score window class

---
+++ add_score.cpp
@@ -0,0 +1,5 @@
+#include "add_score.hpp"
+
+AddScore::AddScore(){
+	//
+}
---
+++ add_score.hpp
@@ -0,0 +1,16 @@
+#ifndef ADD_SCORE_H
+#define ADD_SCORE_H
+
+#include <QWidget>
+
+class AddScore : public QWidget{
+	Q_OBJECT
+
+public:
+	AddScore();
+
+private:
+	//
+};
+
+#endif
